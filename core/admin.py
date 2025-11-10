010b12e516ece4cbfd57576b51d0c70b49f5341b
Lussandrorj2 <167385164+Lussandrorj2@users.noreply.github.com>
2025-11-10 15:59:42 -0300

    chore: atualizações

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tarefa  # 1. Importe seu Model 
 
-# Register your models here.
+# 2. "Registre" seu Model no site de administração 
+admin.site.register(Tarefa) 
